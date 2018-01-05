34ba2ccdea26153dbefdeffcda01a58cfe561c49
Karan Rawal <kr14nov@outlook.com>
2018-01-05 23:37:33 +0530

    Initial commit for parralax background.

---
+++ libgdx/parallaxBackground/desktop/src/com/blurry/parallaxbackground/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.blurry.parallaxbackground.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.blurry.parallaxbackground.ParallaxBackground;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ParallaxBackground(), config);
+	}
+}
